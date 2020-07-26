5bde934e44ac68131f7490a640e60bbc7e3c346e
QRISTINE Sargsyan <qsargsyan96@mail.ru>
2020-07-26 17:15:31 +0400

    Add project files.

---
+++ CheckBrackets/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = "1+(3+2-(2+3)*4-((3+1)*(4-2)))";
+            Stack<int> stack = new Stack<int>();
+
+            bool correctBrackets = true;
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char ch = expression[i];
+
+                if (ch == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (ch == ')')
+                {
+                    if (stack.Count == 0)
+                    {
+                        correctBrackets = false;
+                        break;
+                    }
+                    stack.Pop();                  
+                }
+            }
+            if (stack.Count != 0)
+            {
+                correctBrackets = false;
+            }
+            Console.WriteLine("Are the brackets correct? " + correctBrackets);
+
+        }
+    }
+}
